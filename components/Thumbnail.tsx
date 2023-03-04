1cbc691a6e8f41170df42b66aaa8c483043a10f8
amaguri-star <ryusei@gmail.com>
2023-03-04 19:05:52 +0900

    finally done

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,36 +1,32 @@
+import { DocumentData } from "firebase/firestore";
 import Image from "next/image";
-import { useRecoilState } from 'recoil'
-import React from "react";
+import { useRecoilState } from "recoil";
 import { modalState, movieState } from "../atoms/modalAtoms";
 import { Movie } from "../typings";
 
 interface Props {
-  movie: Movie;
+  movie: Movie | DocumentData;
 }
 
 function Thumbnail({ movie }: Props) {
-  const [showModal, setShowModal] = useRecoilState(modalState)
-  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
   return (
     <div
-      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out
-        md:h-36 md:min-w-[260px] md:hover:scale-105"
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
       onClick={() => {
         setCurrentMovie(movie);
         setShowModal(true);
       }}
     >
       <Image
-        className="rounded-sm object-cover md:rounded"
-        alt="image for a movie"
+        alt="image"
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
         }`}
-        fill
-        sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
       />
     </div>
   );
